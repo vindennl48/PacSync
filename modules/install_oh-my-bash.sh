020ab54bab3dd2753512a71fea5f7b8d3501c592
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2025-07-06 23:49:46 -0400

    Updated module oh-my-bash

--- modules/install_oh-my-bash.sh
+++ modules/install_oh-my-bash.sh
@@ -14,13 +14,22 @@ install() {
 
   sudo -u "$SUDO_USER" bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
 
+  # --- Customization ---
+  local user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+  sed -i 's/^OSH_THEME=.*/OSH_THEME="powerbash10k"/' "${user_home}/.bashrc"
+  echo "export EDITOR=vim" >> "${user_home}/.bashrc"
+  # --- End Customization ---
+
   echo "----> Configuration Complete!"
 }
 
 uninstall() {
   echo "----> Uninstalling Oh-My-Bash..."
 
-  uninstall_oh_my_bash
+  (yes | sudo -u "$SUDO_USER" bash -i -c "uninstall_oh_my_bash") || true
+
+  local user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+  (sudo -u "$SUDO_USER" rm "$user_home"/.bashrc.*) || true
 
   echo "----> Uninstalled!"
 }
